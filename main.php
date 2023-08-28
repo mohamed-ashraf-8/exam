ed435b3064f4cec0817830003c30e76da6b1ed9d
Mohamed Ashraf <109896315+mohamed-ashraf-8@users.noreply.github.com>
2023-08-28 17:32:29 +0300

    Add files via upload

--- main.php
+++ main.php
@@ -1,125 +1,141 @@
-Q1:
-1- 16
-2- undefined
-3- true
-<br>
+Q1<br>
 <?php
-function fibonacci($n) {
-    $fib = array();
-    $fib[0] = 0;
-    $fib[1] = 1;
-    
-    for ($i = 2; $i < $n; $i++) {
-        $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+
+class product
+{
+    public $id;
+    public $name;
+    public $price;
+
+    public function __construct($id, $name, $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
     }
-    
-    return $fib;
+
+    public function DisplayInfo()
+    {
+        echo "Product ID: " . $this->id . "<br>";
+        echo "Product Name: " . $this->name . "<br>";
+        echo "Product Price: $" . $this->price . "<br>";
+    }
 }
 
-$n = 10;
+$product1 = new Product(001, "Product 1", 100);
+$product2 = new Product(002, "Product 2", 200);
+$product3 = new Product(003, "Product 3", 300);
+$product4 = new Product(004, "Product 4", 400);
 
-$result = fibonacci($n);
+$products = [$product1, $product2, $product3, $product4];
 
-echo "Fibonacci Series: ";
-for ($i = 0; $i < $n; $i++) {
-    echo $result[$i] . "<br>";
+$totalPrice = 0;
+foreach ($products as $product) {
+    $totalPrice += $product->price;
 }
+
+foreach ($products as $product) {
+    $product->displayInfo();
+}
+
+$numOfProducts = count($products);
+echo "Number of Products: " . $numOfProducts . "<br>";
+echo "Total Price: $" . $totalPrice . "<br>";
 ?>
-
 <br>
-
+Q2
+<br>
 <?php
-function isLeapYear($year) {
-    if (($year % 4 == 0 && $year % 100 != 0) || $year % 400 == 0) {
-        return true; 
-    } else {
+
+function Anagram($str1, $str2)
+{
+    if (strlen($str1) !== strlen($str2)) {
         return false;
     }
+
+    $sortedStr1 = str_split($str1);
+    sort($sortedStr1);
+    $sortedStr2 = str_split($str2);
+    sort($sortedStr2);
+    return $sortedStr1 === $sortedStr2;
 }
 
-$year = 2024;
+$string1 = "listen";
+$string2 = "silent";
 
-if (isLeapYear($year)) {
-    echo $year . " is a leap year.";
+if (Anagram($string1, $string2)) {
+    echo "The strings '$string1' and '$string2' are anagrams.";
 } else {
-    echo $year . " is not a leap year.";
+    echo "The strings '$string1' and '$string2' are not anagrams.";
 }
 ?>
 
 <br>
-
+<br>
+Q3
+<br>
 <?php
-function generateUniqueRandomNumbers($start, $end, $count) {
-    $range = range($start, $end);
-    $randomNumbers = array();
-    
-    if ($count > ($end - $start + 1)) {
-        echo "Error: Cannot generate more unique random numbers than the specified range.";
-        return;
-    }
-    
-    for ($i = 0; $i < $count; $i++) {
-        $index = mt_rand(0, count($range) - 1);
-        $randomNumbers[] = $range[$index];
-        array_splice($range, $index, 1);
-    }
-    
-    return $randomNumbers;
+function Palindrome($str)
+{
+    $reverseStr = strrev($str);
+    return $str == $reverseStr;
 }
 
-$start = 1;
-$end = 10;
-$count = 5;
+$string = "eye";
 
-$randomNumbers = generateUniqueRandomNumbers($start, $end, $count);
-
-echo "Generated Unique Random Numbers: ";
-foreach ($randomNumbers as $number) {
-    echo $number . " ";
+if (Palindrome($string)) {
+    echo "The string '$string' is a palindrome.";
+} else {
+    echo "The string '$string' is not a palindrome.";
 }
 ?>
 
 <br>
+<br>
+Q4
+<br>
+<?php 
+$jsonData = file_get_contents('settings.json');
 
+$data = json_decode($jsonData, true);
+
+foreach ($data as $item) {
+    $name = $item['name'];
+    $age = $item['age'];
+    echo "Name: $name, Age: $age<br>";
+}?>
+
+<br>
+<br>
+Q5
+<br>
 <?php
-echo "Multiplication Table up to 5x5:\n\n";
 
-for ($i = 1; $i <= 5; $i++) {
-    for ($j = 1; $j <= 5; $j++) {
-        echo $i . " x " . $j . " = " . ($i * $j) . "<br>";
-    }
-    echo "\n";
+trait Trait1
+{
+    public function methodA() {}
 }
+
+trait Trait2 
+{
+    public function methodB() {}
+}
+
+class MyClass 
+{
+    use Trait1, Trait2;
+}
+
+$obj = new MyClass();
+$obj->methodA(); 
+$obj->methodB(); 
 ?>
 
 <br>
-
-<?php
-function removeDuplicates($sortedList) {
-    $uniqueList = array();
-    $previousItem = null;
-
-    foreach ($sortedList as $item) {
-        if ($item !== $previousItem) {
-            $uniqueList[] = $item;
-        }
-        $previousItem = $item;
-    }
-
-    return $uniqueList;
-}
-
-$sortedList = array(1, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6);
-
-$uniqueList = removeDuplicates($sortedList);
-
-echo "Original Sorted List: " . implode(", ", $sortedList) . "\n";
-echo "List with Duplicates Removed: " . implode(", ", $uniqueList) . "\n";
-?>
 <br>
-Q3:
-1-false
-2-false
-3-true
-4-true
-5-true
+Q6
+Overloading means having multiple methods of the same name but different parameters. Overriding means replacing a parent
+method.
+lack of access control, late static binding, procedural nature of methods and no concept of replacing parent methods,
+are reasons why PHP does not natively support classical method overriding found in other languages.
+<br>
